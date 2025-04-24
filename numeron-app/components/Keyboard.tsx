7dc331f6ab192b59745415f4ae8a474bab47c3bf
M-Yusei1001 <bitterchocolate1001@gmail.com>
2025-04-24 06:15:08 +0000

    wip: キーボード整形

--- numeron-app/components/Keyboard.tsx
+++ numeron-app/components/Keyboard.tsx
@@ -7,16 +7,20 @@ export default function Keyboard({
 }: KeyboardProps) {
 	const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
+	const btnConfig = "flex cursor-pointer select-none";
+
 	return (
-		<div>
+		<div className="grid grid-cols-3 gap-4 max-w-sm mx-auto p-4 border border-gray-300 rounded-lg bg-gray-100 shadow-md">
 			{numbers.map((number) => (
-				<button
-					key={number}
-					onClick={() => onNumberClick(number)}
-					className={`${clickedNumbers.includes(number) ? "btn btn-primary" : "btn"}`}
-				>
-					{number}
-				</button>
+				<div className="flex justify-center items-center" key={number}>
+					<button
+						key={number}
+						onClick={() => onNumberClick(number)}
+						className={`btn btn-square ${btnConfig} ${clickedNumbers.includes(number) ? "btn-primary" : ""}`}
+					>
+						{number}
+					</button>
+				</div>
 			))}
 		</div>
 	);
